592c5bd5ab41829155b1ee57ac103412a9a36327
unknown <hassett.n1@gmail.com>
2024-09-21 23:21:06 -0400

    Add basic API functionality, with root and /book endpoints

---
+++ bookshop_app/crud.py
@@ -0,0 +1,7 @@
+# bookshop_app/crud.py
+# create update delete
+
+# from sqlalchemy.orm import Session
+# from . import keygen, models, schemas
+
+# TODO: move create-update-delete functionality here
--- bookshop_app/main.py
+++ bookshop_app/main.py
@@ -1,16 +1,17 @@
 # bookshop_app/main.py
 
-import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
+import secrets
 
 from sqlalchemy.orm import Session
 
 from . import models, schemas, crud
 from .database import SessionLocal, engine
 
-# TODO: setup FastAPI
+# setup FastAPI
+shop_app = FastAPI()
 
-# TODO: setup database
+# setup database
 models.Base.metadata.create_all(bind=engine)
 
 def get_db():
@@ -22,8 +23,25 @@ def get_db():
 
 # TODO: add testing for database functionality
 
+
+@shop_app.get("/")
+def read_root():
+    return "Welcome to my bookshop :)"
+
+
 # TODO: setup post endpoint with function to add book to inventory
 # TODO: add functionality to update stock numbers also
+@shop_app.post("/book", )  # add response_model for response
+def create_book(book: schemas.StockBase, db: Session = Depends(get_db)):
+    # not sure i need this key but lets keep it for now
+    key = "".join(secrets.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(5))
+    db_book = models.Stock(
+        title=book.title, author=book.author,
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
 
 # TODO: setup post endpoint to create orders and add these to an orders database
 # TODO: add functionality to remove stock from inventory according to orders
