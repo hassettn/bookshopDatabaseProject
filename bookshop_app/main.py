161cb41c72ea86710408cfede54ec243f6f75854
unknown <hassett.n1@gmail.com>
2024-10-14 17:28:21 -0400

    Add functionality to delete stock

--- bookshop_app/main.py
+++ bookshop_app/main.py
@@ -15,7 +15,7 @@ shop_app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-#why do it this way and not with sessionmaker?
+# why do it this way and not with sessionmaker?
 def get_db():
     db = SessionLocal()
     try:
@@ -63,10 +63,10 @@ def create_book(book: schemas.Stock, db: Session = Depends(get_db)):
 # endpoint to add an order to the orders table
 @shop_app.post("/order")  # add response_model for response
 def create_order(order: schemas.Orders, db: Session = Depends(get_db)):
-    if book:=crud.get_book_by_author_title(db=db, author=order.author, title=order.title,):
+    if book := crud.get_book_by_author_title(db=db, author=order.author, title=order.title,):
         if book.stock < order.quantity:
             return "not enough books!"
-        #add order details to order table
+        # add order details to order table
         key = random_key(5)
         db_order = models.Order(
             title=order.title,
@@ -80,10 +80,10 @@ def create_order(order: schemas.Orders, db: Session = Depends(get_db)):
 
         # to update stock levels in stock table
         db_book = (db.query(models.Stock)
-                   .filter(models.Stock.author==order.author, models.Stock.title==order.title)
+                   .filter(models.Stock.author == order.author, models.Stock.title == order.title)
                    .first())
         setattr(db_book, 'stock', book.stock - order.quantity)
-        db.commit()  # should i change db to session?
+        db.commit()  # should I change db to session?
         # db.refresh(db_book)  # what does db.refresh do?
 
         db.refresh(db_order)
@@ -111,15 +111,31 @@ def get_orders(db: Session = Depends(get_db), ):
 
 
 @shop_app.post("/book/restock")
-def restock_book(title, author, quantity:int, db: Session = Depends(get_db), ):
-    #add a number of stock to a book listing which already exists
-    db_book= crud.get_book_by_author_title(db=db, author=author, title=title, )
+def restock_book(title, author, quantity: int, db: Session = Depends(get_db), ):
+    # add a number of stock to a book listing which already exists
+    # TODO: update with := operator
+    db_book = crud.get_book_by_author_title(db=db, author=author, title=title, )
     if db_book:
-        #then update the stock
+        # this should not be necessary, why would the value be null
+        if not db_book.stock:
+            db_book.stock = 10
+        # then update the stock
         db_book.stock = int(db_book.stock) + quantity
-        db_book.in_stock=bool(db_book.stock)
+        db_book.in_stock = bool(db_book.stock)
         db.commit()
         db.refresh(db_book)
         return db_book
     else:
         return "book does not yet exist,please add using /book endpoint"
+
+
+@shop_app.post("/book/delete")
+def delete_book(title: str, author: str, id: int, db: Session = Depends(get_db), ):
+    # TODO: create function in crud for this
+    if db.query(models.Stock).filter(models.Stock.id == id).first():
+        crud.delete_book_by_id(db, book_id=id, author=author, title=title)
+        db.commit()
+        db.refresh()
+        return "book deleted successfully"
+    else:
+        return "book doesn't exist, delete something else you fool"
